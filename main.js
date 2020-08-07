6a8110d9da074a7726e0c69f109fdde076c03a04
Boris84 <simon.789@hotmail.co.uk>
2020-08-07 14:20:21 +0100

    update 265 fruitbox mobile - fix update container

--- main.js
+++ main.js
@@ -2464,7 +2464,7 @@ let randFruit = Math.floor(Math.random() * count);
         let query = window.matchMedia("(max-width: 600px)");
      
         if (query.matches) {
-          container.style.height = "910px";
+          container.style.height = "890px";
         }
 
         guessButton.style.border = 'none';
